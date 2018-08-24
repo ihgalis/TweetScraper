7aa165bf391d6be5031089016d3f83979b23baca
Andre Fritsche <andre.fritsche@outlook.de>
2018-08-24 13:51:46 +0200

    to make it possible to know which keywords have been used to find specific tweets.

--- TweetScraper/items.py
+++ TweetScraper/items.py
@@ -28,6 +28,7 @@ class Tweet(Item):
     has_media = Field() # True/False, whether a tweet contains media (e.g. summary)
     medias = Field()    # a list of media
 
+    query = Field()     # search string which was used to find
 
 class User(Item):
     ID = Field()            # user id
--- TweetScraper/spiders/TweetCrawler.py
+++ TweetScraper/spiders/TweetCrawler.py
@@ -64,6 +64,7 @@ class TweetScraper(CrawlSpider):
             try:
                 tweet = Tweet()
 
+                tweet['query'] = self.query
                 tweet['usernameTweet'] = item.xpath('.//span[@class="username u-dir u-textTruncate"]/b/text()').extract()[0]
 
                 ID = item.xpath('.//@data-tweet-id').extract()
